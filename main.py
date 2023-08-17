18842b444d339482fc2d893995712c9a7087dc7d
gmbgu <gmb.gustavo@gmail.com>
2023-08-17 02:45:38 -0300

    Colors

--- main.py
+++ main.py
@@ -25,17 +25,9 @@ if __name__ == '__main__':
     print(f'\nCriando seus jogos, isto pode levar até 20 segundos a depender da quantidade de apostas.')
     print(f'Inicialização de entropia...\n')
     modalidade = 'Megasena'
-    aposta1 = Megasena(dezenas=7)   # (surpresinha automatica para faltantes)
-    aposta2 = Megasena(dezenas=7)
-    aposta3 = Megasena(dezenas=7)
-    aposta4 = Megasena(dezenas=7)
-    aposta5 = Megasena(dezenas=7)
-    aposta6 = Megasena(dezenas=7)
-    aposta7 = Megasena(dezenas=7)
-    aposta8 = Megasena(dezenas=7)
-    volante = [aposta1.jogo, aposta2.jogo, aposta3.jogo,
-               aposta4.jogo, aposta5.jogo, aposta6.jogo,
-               aposta7.jogo, aposta8.jogo]
+    aposta1 = Megasena(1, 5, 6, 15, 22, 28, dezenas=6)   # (surpresinha automatica para faltantes)
+    aposta2 = Megasena(dezenas=6)
+    volante = [aposta1.jogo, aposta2.jogo]
     print(f'Suas apostas: {volante[0:2]} ...')   # Apresenta a aposta ao usuario
     print(f'Quantidade de dezenas: {len(aposta1)}')
     print(f'Modalidade: {modalidade}')
@@ -46,12 +38,13 @@ if __name__ == '__main__':
     # executando o método sortear()
     # Deve ser informado o parametro ao metodo conferir() a propriedade jogo do ojbeto de aposta, Megasena, Quina...
     analise = {'modalidade': modalidade, 'dezenas': len(aposta1), 'concursos': 0, 'apostas': len(volante)}
-    for stat in range(100):
+    for stat in range(1000):
         # Medição de desempenho
         start_time = time.time()
         estatistica = Salvadados(dados=analise)
         concursos = 1
         resultado_loteria = concurso_loteria.sortear()    # Primeiro sorteio
+        print(Fore.RED + f'Iteração {stat}' + Fore.RESET)
         while True not in concurso_loteria.conferir(volante):
             resultado_loteria = concurso_loteria.sortear()    # Novo sorteio
             concursos += 1                                    # Controla o numero de concursos
